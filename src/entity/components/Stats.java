67c3ad6d20c353d819e7243a434d10ffbfbafb3b
Cmanchik <aksenova1648@gmail.com>
2019-04-11 22:20:36 +0400

    Добавил Entity, Stats

---
+++ src/entity/components/Stats.java
@@ -0,0 +1,14 @@
+package entity.components;
+
+public class Stats {
+	int maxHealPoint;
+	int currentHealPoint;
+	
+	int maxManaPoint;
+	int currentManaPoint; 
+	
+	int attackTime;
+	int attackPower;
+	
+	int speed;
+}
