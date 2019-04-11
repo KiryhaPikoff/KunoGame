4143aca7323d1190acac354863aa9f0be415ca1b
KiryaPikoff <har4ok@bk.ru>
2019-04-12 00:48:46 +0300

    Тест KeyListenera

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,10 +1,11 @@
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 
 import map.Chunk;
 import map.TileType;
@@ -13,20 +14,26 @@ import utils.Constants;
 public class MainWindow extends JFrame {
 
 	private JPanel contentPane;
+	private Chunk testChunk;
 	
 	public MainWindow() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(Constants.WINDOW_POZITION_X, Constants.WINDOW_POZITION_Y,
 				  Constants.WINDOW_WIDTH, Constants.WINDOW_HEIGHT);
 		contentPane = new Renderer();
+		setResizable(false);
 		setContentPane(contentPane);
 		setVisible(true);
+		
+		
+		testChunk = new Chunk("10000000");
+		this.addKeyListener(testChunk.getKeyListener());
 	}
 	
 	
 	public void paint(Graphics g) {
 		super.paint(g);
-		Renderer.drawObject(new Chunk("10010000"));
+		Renderer.drawObject(testChunk);
 		g.drawImage(Renderer.canvas, 10, 32, null);
 	}
 }
--- src/map/TileType.java
+++ src/map/TileType.java
@@ -5,6 +5,7 @@ import java.awt.Image;
 import utils.ImageLoader;
 
 public enum TileType {
+	
 	ground("ground", ImageLoader.loadImage("resources/images/ground64t.png")),
 	water("water", ImageLoader.loadImage("resources/images/water64t.png")),
 	road("road", ImageLoader.loadImage("resources/images/road64t.png")),
