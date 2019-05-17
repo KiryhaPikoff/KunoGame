48f3e52281d36f2a57f096cf5612827210e0e517
Cmanchik <aksenova1648@gmail.com>
2019-05-17 23:08:05 +0400

    исправил баг с преследованием монстром игрока

--- src/myTestPackage/components/direction/DirectionPursuit.java
+++ src/myTestPackage/components/direction/DirectionPursuit.java
@@ -4,26 +4,29 @@ import myTestPackage.entity.Entity;
 import myTestPackage.entity.monster.Monster;
 
 public abstract class DirectionPursuit {
+
+
     public static DirectionMovement chase(Entity monster,Entity target) {
+        DirectionMovement directionMovement = DirectionMovement.STAND;
 
+        if (target.getCoordinates().getX() < monster.getCoordinates().getX()) { directionMovement = DirectionMovement.WEST; }
+        if (target.getCoordinates().getX() > monster.getCoordinates().getX()) { directionMovement = DirectionMovement.EAST; }
+        if (target.getCoordinates().getY() < monster.getCoordinates().getY()) { directionMovement = DirectionMovement.NORTH; }
+        if (target.getCoordinates().getY() > monster.getCoordinates().getY()) { directionMovement = DirectionMovement.SOUTH; }
 
         if (target.getCoordinates().getY() < monster.getCoordinates().getY() &&
-                target.getCoordinates().getX() < monster.getCoordinates().getY()) { return DirectionMovement.NORTH_WEST; }
+                target.getCoordinates().getX() < monster.getCoordinates().getX()) { directionMovement = DirectionMovement.NORTH_WEST; }
         if (target.getCoordinates().getY() < monster.getCoordinates().getY() &&
-                target.getCoordinates().getX() > monster.getCoordinates().getY()) { return DirectionMovement.NORTH_EAST; }
-
+                target.getCoordinates().getX() > monster.getCoordinates().getX()) { directionMovement = DirectionMovement.NORTH_EAST; }
         if (target.getCoordinates().getY() > monster.getCoordinates().getY() &&
-                target.getCoordinates().getX() < monster.getCoordinates().getY()) { return DirectionMovement.SOUTH_WEST; }
+                target.getCoordinates().getX() < monster.getCoordinates().getX()) { directionMovement = DirectionMovement.SOUTH_WEST; }
         if (target.getCoordinates().getY() > monster.getCoordinates().getY() &&
-                target.getCoordinates().getX() > monster.getCoordinates().getY()) { return DirectionMovement.SOUTH_EAST; }
-
-        if (target.getCoordinates().getX() < monster.getCoordinates().getX()) { return DirectionMovement.WEST; }
-        if (target.getCoordinates().getX() > monster.getCoordinates().getX()) { return DirectionMovement.EAST; }
-        if (target.getCoordinates().getY() < monster.getCoordinates().getY()) { return DirectionMovement.NORTH; }
-        if (target.getCoordinates().getY() > monster.getCoordinates().getY()) { return DirectionMovement.SOUTH; }
+                target.getCoordinates().getX() > monster.getCoordinates().getX()) { directionMovement = DirectionMovement.SOUTH_EAST; }
 
 
 
-        return DirectionMovement.STAND;
+
+
+        return directionMovement;
     }
 }
